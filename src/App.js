21fb349734fba1cc022fd733c290c9e9b9d3c01e
angsabrina <angsabrina98@yahoo.com>
2020-11-07 19:50:03 -0800

    connection to backend server made in ApiCall func

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
 import React from 'react';
 import './App.css';
-import NamesContainer from './NamesContainer';
 import baron from './baron.jpg';
 
 class App extends React.Component {
 
   state = {
     names: [
-      {'id': 1, 'name':'Julie', 'recent_match_time':'2020-11-06', 'KDA':'10/3/5'},
-      {'id': 2, 'name':'Kevin', 'recent_match_time':'2020-10-16', 'KDA':'4/20/1'}, 
-      {'id': 3, 'name':'Dennis', 'recent_match_time':'2020-10-24', 'KDA':'15/13/5'}, 
-      {'id': 4, 'name':'Sabrina', 'recent_match_time':'2020-11-01', 'KDA':'8/8/8'},
+      {'id': 1, 'name':'Julie', 'recent_match_time':'2020-11-06', 'KDA':'10/3/5', 'tilt_level':'1'},
+      {'id': 2, 'name':'Kevin', 'recent_match_time':'2020-10-16', 'KDA':'4/20/1', 'tilt_level':'5'}, 
+      {'id': 3, 'name':'Dennis', 'recent_match_time':'2020-10-24', 'KDA':'15/13/5', 'tilt_level':'3'}, 
+      {'id': 4, 'name':'Sabrina', 'recent_match_time':'2020-11-01', 'KDA':'8/8/8', 'tilt_level':'2'},
     ],
-    searchTerm: ''
+    searchTerm: '', 
+    searchOutput: '',
+    searchValue: "",
+    tilt: ''
   }
 
-  editSearchTerm = (e) => {
-    this.setState({searchTerm: e.target.value})
-  }
+  handleOnChange = event => {
+    this.setState({ searchValue: event.target.value });
+  };
 
-  dynamicSearch = () => {
-    var output = this.state.names.filter(element => element.name.toLowerCase().includes(this.state.searchTerm.toLowerCase()))
-    console.log(output)
-    return output
-  }
+  handleSearch = () => {
+    this.makeApiCall(this.state.searchValue);
+  };
 
+  makeApiCall = searchInput => {
+    var searchUrl = `https://tiltproof-backend.herokuapp.com/gettilt`;
+    fetch(searchUrl)
+      .then(response => {
+        return response.json();
+      })
+      .then(jsonData => {
+        console.log(jsonData)
+        this.setState({ tilt: jsonData });
+      });
+  };
 
     render(){
       return (
@@ -32,11 +43,23 @@ class App extends React.Component {
           <div class="App-logo">
             <img src={baron} alt="" height="600" width="300" float="left"></img>
           </div>
+
           <div style = {{textAlign: 'center', paddingTop: '30vh'}}>
-          <input type= 'text' value = {this.state.searchTerm} onChange = {this.editSearchTerm} placeholder = 'Search for a user!'/>
-          <br></br>
-          <h3>These are the important names:</h3>
-          <NamesContainer names = {this.dynamicSearch()}/>
+          <input
+            name="text"
+            type="text"
+            placeholder="Search"
+            onChange={event => this.handleOnChange(event)}
+            value={this.state.searchValue}
+          />
+          <button onClick={this.handleSearch}>Search</button>
+          {this.state.tilt ? (
+            <div >
+              {this.state.tilt}
+            </div>
+            ) : (
+              <p>Try searching a summoner name</p>
+            )}
           </div>
         </div>
       );
